42651e77dc530994b481f59da15f66e57a85def1
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-11-12 18:19:02 +0100

    *

---
+++ mvc-demo/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace mvc_demo.Controllers
+{
+    public class LoggedInUser
+    {
+        public string Name { get; set; }
+    }
+
+    public class NewUser
+    {
+        public string Name { get;set; }
+        public bool IsAdmin { get; set; }
+    }
+    public class UserBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var loggedInUser = new LoggedInUser()
+            {
+                Name = "De Yoeri"
+            };
+            return loggedInUser;
+        }
+    }
+
+    public class UserController : Controller
+    {
+        public ActionResult Index(LoggedInUser user) => Content(user.Name);
+
+        public ActionResult NewUser([Bind(Exclude = nameof(Controllers.NewUser.IsAdmin))]NewUser user)
+        {
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ mvc-demo/Controllers/ValidationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace mvc_demo.Controllers
+{
+    public class NewEntity : IValidatableObject
+    {
+        [Required] public string Name { get; set; }
+        [Required] public string Location { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Location == "Brugge") yield return new ValidationResult("West-Vlamingen niet welkom!");
+        }
+    }
+
+    public class ValidationController : Controller
+    {
+        public ActionResult Index(NewEntity entity)
+        {
+            if (!ModelState.IsValid)
+                return Content("Bad request  mboy");
+            return Json(entity, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult MyView(NewEntity entity)
+        {
+            if(!ModelState.IsValid) ModelState.AddModelError(string.Empty, "Fix errors please");
+            return View(entity);
+        }
+    }
+}
