a8ea4ed27fac451eb573a1feb3b14f7d7e518fbc
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-11-14 09:49:40 +0100

    owin

---
+++ mvc-demo/Startup.cs
@@ -0,0 +1,16 @@
+﻿using mvc_demo;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace mvc_demo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // New code:
+            app
+        }
+    }
+}
